fc1a03149e34dad634e7f0f71f8fae81148d60e0
David Padilla <david@crowdint.com>
2011-03-31 19:37:51 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Curso::Application.config.secret_token = 'b8b0d0d07d7914be2a6491a0005f32cfcaa4a55174ae26d96d0845bef63e78466925c05990a2efe812f04f038a75ca2256596e532b08f25e8b7074056127ed5b'
