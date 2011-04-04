a0b9c7bc59fb14fcde801e665977d7b2d8a7c62d
David Padilla <david@crowdint.com>
2011-04-03 21:47:48 -0500

    Removed unused migration

--- db/migrate/20110401025721_add_view_count_to_posts.rb
+++
@@ -1,9 +0,0 @@
-class AddViewCountToPosts < ActiveRecord::Migration
-  def self.up
-    add_column :posts, :view_count, :integer
-  end
-
-  def self.down
-    remove_column :posts, :view_count
-  end
-end
